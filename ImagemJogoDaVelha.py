9aa1e15e6dfeded79d5fae4c58819707aeaf5229
gestevam <gestevam@DCCNOTE61>
2019-06-17 10:33:46 -0300

    Realizando validação para todas as imagens

--- ImagemJogoDaVelha.py
+++ ImagemJogoDaVelha.py
@@ -3,27 +3,27 @@ import numpy as np
 from sklearn.svm import SVC
 from sklearn.svm import SVR
 
-valido01_new = cv2.imread('C:/Users/guilh/Pictures/Validos/JogoDaVelha.png')
-valido02_new = cv2.imread('C:/Users/guilh/Pictures/Validos/JogoDaVelha02.png')
-valido03_new = cv2.imread('C:/Users/guilh/Pictures/Validos/JogoDaVelha04.png')
-valido04_new = cv2.imread('C:/Users/guilh/Pictures/Validos/JogoDaVelha07.png')
-valido05_new = cv2.imread('C:/Users/guilh/Pictures/Validos/JogoDaVelha09.png')
-valido06_new = cv2.imread('C:/Users/guilh/Pictures/Validos/JogoDaVelha15.png')
-valido07_new = cv2.imread('C:/Users/guilh/Pictures/Validos/JogoDaVelha16.png')
-valido08_new = cv2.imread('C:/Users/guilh/Pictures/Validos/JogoDaVelha17.png')
-valido09_new = cv2.imread('C:/Users/guilh/Pictures/Validos/JogoDaVelha18.png')
-valido10_new = cv2.imread('C:/Users/guilh/Pictures/Validos/JogoDaVelha19.png')
+valido01_new = cv2.imread('Validos/JogoDaVelha.png')
+valido02_new = cv2.imread('Validos/JogoDaVelha02.png')
+valido03_new = cv2.imread('Validos/JogoDaVelha04.png')
+valido04_new = cv2.imread('Validos/JogoDaVelha07.png')
+valido05_new = cv2.imread('Validos/JogoDaVelha09.png')
+valido06_new = cv2.imread('Validos/JogoDaVelha15.png')
+valido07_new = cv2.imread('Validos/JogoDaVelha16.png')
+valido08_new = cv2.imread('Validos/JogoDaVelha17.png')
+valido09_new = cv2.imread('Validos/JogoDaVelha18.png')
+valido10_new = cv2.imread('Validos/JogoDaVelha19.png')
 
-invalido01_new = cv2.imread('C:/Users/guilh/Pictures/Invalidos/JogoDaVelha01.png')
-invalido02_new = cv2.imread('C:/Users/guilh/Pictures/Invalidos/JogoDaVelha03.png')
-invalido03_new = cv2.imread('C:/Users/guilh/Pictures/Invalidos/JogoDaVelha05.png')
-invalido04_new = cv2.imread('C:/Users/guilh/Pictures/Invalidos/JogoDaVelha06.png')
-invalido05_new = cv2.imread('C:/Users/guilh/Pictures/Invalidos/JogoDaVelha08.png')
-invalido06_new = cv2.imread('C:/Users/guilh/Pictures/Invalidos/JogoDaVelha10.png')
-invalido07_new = cv2.imread('C:/Users/guilh/Pictures/Invalidos/JogoDaVelha11.png')
-invalido08_new = cv2.imread('C:/Users/guilh/Pictures/Invalidos/JogoDaVelha12.png')
-invalido09_new = cv2.imread('C:/Users/guilh/Pictures/Invalidos/JogoDaVelha13.png')
-invalido10_new = cv2.imread('C:/Users/guilh/Pictures/Invalidos/JogoDaVelha14.png')
+invalido01_new = cv2.imread('Invalidos/JogoDaVelha01.png')
+invalido02_new = cv2.imread('Invalidos/JogoDaVelha03.png')
+invalido03_new = cv2.imread('Invalidos/JogoDaVelha05.png')
+invalido04_new = cv2.imread('Invalidos/JogoDaVelha06.png')
+invalido05_new = cv2.imread('Invalidos/JogoDaVelha08.png')
+invalido06_new = cv2.imread('Invalidos/JogoDaVelha10.png')
+invalido07_new = cv2.imread('Invalidos/JogoDaVelha11.png')
+invalido08_new = cv2.imread('Invalidos/JogoDaVelha12.png')
+invalido09_new = cv2.imread('Invalidos/JogoDaVelha13.png')
+invalido10_new = cv2.imread('Invalidos/JogoDaVelha14.png')
 
 valido01 = cv2.resize(valido01_new, (10,10))
 valido02 = cv2.resize(valido02_new, (10,10))
@@ -67,62 +67,63 @@ classifier_linear.fit(X,Y)
 print('Finished train')
 print(40 * '-')
 
-prediction = classifier_linear.predict(invalido01.reshape(1,-1))
+i  = 1;
+for x in X : 
+	prediction = classifier_linear.predict(x.reshape(1,-1))
 
-score = classifier_linear.score(X,Y)
+	score = classifier_linear.score(X,Y)
 
-print('Result: {}'.format(prediction))
-print('Score of precision: {:.1f}%'.format(score * 100))
+	print('Result: {}'.format(prediction))
+	print('Score of precision: {:.1f}%'.format(score * 100))
 
-if prediction <= 10 :
-    print('A Imagem é Valida!')
-elif prediction > 10 :
-	print('A Imagem é invalida!')
+	if prediction == 1:
+		result = valido01_new
+	elif prediction == 2:
+		result = valido02_new
+	elif prediction == 3:
+		result = valido03_new
+	elif prediction == 4:
+		result = valido04_new
+	elif prediction == 5:
+		result = valido05_new
+	elif prediction == 6:
+		result = valido06_new
+	elif prediction == 7:
+		result = valido07_new
+	elif prediction == 8:
+		result = valido08_new
+	elif prediction == 9:
+		result = valido09_new
+	elif prediction == 10:
+		result = valido10_new
+	elif prediction == 11:
+		result = invalido01_new
+	elif prediction == 12:
+		result = invalido02_new
+	elif prediction == 13:
+		result = invalido03_new
+	elif prediction == 14:
+		result = invalido04_new
+	elif prediction == 15:
+		result = invalido05_new
+	elif prediction == 16:
+		result = invalido06_new
+	elif prediction == 17:
+		result = invalido07_new
+	elif prediction == 18:
+		result = invalido08_new
+	elif prediction == 19:
+		result = invalido09_new
+	elif prediction == 20:
+		result = invalido10_new
 
-if prediction == 1:
-	result = valido01_new
-elif prediction == 2:
-	result = valido02_new
-elif prediction == 3:
-	result = valido03_new
-elif prediction == 4:
-	result = valido04_new
-elif prediction == 5:
-	result = valido05_new
-elif prediction == 6:
-    result = valido06_new
-elif prediction == 7:
-	result = valido07_new
-elif prediction == 8:
-	result = valido08_new
-elif prediction == 9:
-	result = valido09_new
-elif prediction == 10:
-	result = valido10_new
-elif prediction == 11:
-    result = invalido01_new
-elif prediction == 12:
-	result = invalido02_new
-elif prediction == 13:
-	result = invalido03_new
-elif prediction == 14:
-	result = invalido04_new
-elif prediction == 15:
-	result = invalido05_new
-elif prediction == 16:
-    result = invalido06_new
-elif prediction == 17:
-	result = invalido07_new
-elif prediction == 18:
-	result = invalido08_new
-elif prediction == 19:
-	result = invalido09_new
-elif prediction == 20:
-	result = invalido10_new
+	if prediction <= 10 :
+		print('A Imagem é Valida!')
+		cv2.imshow("A Imagem "+str(i) + " e Valida!", result)
+		i+=1
+	elif prediction > 10 :
+		print('A Imagem é invalida!')
+		cv2.imshow("A Imagem "+str(i) + " e Invalida!", result)
+		i+=1
 
-cv2.imshow("Result", result)
-
-cv2.imshow("Test", invalido01)
 cv2.waitKey(0)
-
-print('---------------------------------------')
